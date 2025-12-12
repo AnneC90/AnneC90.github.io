ccf3a80ee5629516f9a9c696c2ccb83d18434a84
AnneC90 <cortez457@wearescpps.org>
2025-12-12 15:01:22 +0000

    runtime

--- fsd-projects/runtime/js/runLevels.js
+++ fsd-projects/runtime/js/runLevels.js
@@ -1,49 +1,95 @@
 var runLevels = function (window) {
-  window.opspark = window.opspark || {};
-
-  var draw = window.opspark.draw;
-  var createjs = window.createjs;
-  let currentLevel = 0;
-
-  window.opspark.runLevelInGame = function (game) {
-    // some useful constants
-    var groundY = game.groundY;
-
-    // this data will allow us to define all of the
-    // behavior of our game
-    var levelData = window.opspark.levelData;
-
-    // set this to true or false depending on if you want to see hitzones
-    game.setDebugMode(true);
-
-    // TODOs 5 through 11 go here
-    // BEGIN EDITING YOUR CODE HERE
-
-    
-
-    function startLevel() {
-      // TODO 13 goes below here
+ window.opspark = window.opspark || {};
 
 
+ var draw = window.opspark.draw;
+ var createjs = window.createjs;
+ let currentLevel = 0;
 
-      //////////////////////////////////////////////
-      // DO NOT EDIT CODE BELOW HERE
-      //////////////////////////////////////////////
-      if (++currentLevel === levelData.length) {
-        startLevel = () => {
-          console.log("Congratulations!");
-        };
-      }
-    }
-    startLevel();
+
+ window.opspark.runLevelInGame = function (game) {
+   // some useful constants
+   var groundY = game.groundY;
+
+
+   // this data will allow us to define all of the
+   // behavior of our game
+   var levelData = window.opspark.levelData;
+
+
+   // set this to true or false depending on if you want to see hitzones
+   game.setDebugMode(true);
+
+
+   // TODOs 5 through 11 go here
+   // BEGIN EDITING YOUR CODE HERE
+  function createSawBlade (x,y){
+   var hitZoneSize = 25; //gray circle
+   var damageFromObstacle = 10;
+   var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
+   sawBladeHitZone.x = x;
+   sawBladeHitZone.y = y;
+   game.addGameItem(sawBladeHitZone);
+   var obstacleImage = draw.bitmap("img/sawblade.png");
+   sawBladeHitZone.addChild(obstacleImage);
+   obstacleImage.y  = -25
+   obstacleImage.x  = -25
+  }
+   createSawBlade(400, 325)
+  createSawBlade(700, 200)
+     createSawBlade(700, 370)
+ var enemy = game.createGameItem("enemy", 25);
+ var redSquare = draw.rect(50, 50, "red");
+   redSquare.x = -25;
+   redSquare.y = -25;
+   enemy.addChild(redSquare);
+   enemy.x = 400;
+   enemy.y = groundY - 50;
+  game.addGameItem(enemy);
+  enemy.velocityX = -1
+  enemy.rotationalVelocity = 50000
+  enemy.onPlayerCollision = function () {game.changeIntegrity(-10)
+
+
   };
+
+
+
+
+
+
+   function startLevel() {
+     // TODO 13 goes below here
+
+
+
+
+
+
+     //////////////////////////////////////////////
+     // DO NOT EDIT CODE BELOW HERE
+     //////////////////////////////////////////////
+     if (++currentLevel === levelData.length) {
+       startLevel = () => {
+         console.log("Congratulations!");
+       };
+     }
+   }
+   startLevel();
+ };
 };
+
 
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
 if (
-  typeof process !== "undefined" &&
-  typeof process.versions.node !== "undefined"
+ typeof process !== "undefined" &&
+ typeof process.versions.node !== "undefined"
 ) {
-  // here, export any references you need for tests //
-  module.exports = runLevels;
+ // here, export any references you need for tests //
+ module.exports = runLevels;
 }
+
+
+
+
+
--- fsd-projects/runtime/js/view/background.js
+++ fsd-projects/runtime/js/view/background.js
@@ -1,127 +1,138 @@
 var background = function (window) {
-    'use strict';
-    
-    window.opspark = window.opspark || {};
-    var draw = window.opspark.draw;
-    var createjs = window.createjs;
-    
-    /*
-     * Create a background view for our game application
-     */
-    window.opspark.makeBackground = function(app,ground) {
-        /* Error Checking - DO NOT DELETE */
-        if(!app) {
-            throw new Error("Invalid app argument");
-        }
-        if(!ground || typeof(ground.y) == 'undefined') {
-            throw new Error("Invalid ground argument");
-        }
-        
-        // useful variables
-        var canvasWidth = app.canvas.width;
-        var canvasHeight = app.canvas.height;
-        var groundY = ground.y;
-        
-        // container which will be returned
-        var background;
-        
-        //////////////////////////////////////////////////////////////////
-        // ANIMATION VARIABLES HERE //////////////////////////////////////
-        //////////////////////////////////////////////////////////////////
-        // TODO (several):
-      var tree
-      var buildings = []
+   'use strict';
+  
+   window.opspark = window.opspark || {};
+   var draw = window.opspark.draw;
+   var createjs = window.createjs;
+  
+   /*
+    * Create a background view for our game application
+    */
+   window.opspark.makeBackground = function(app,ground) {
+       /* Error Checking - DO NOT DELETE */
+       if(!app) {
+           throw new Error("Invalid app argument");
+       }
+       if(!ground || typeof(ground.y) == 'undefined') {
+           throw new Error("Invalid ground argument");
+       }
       
-        // called at the start of game and whenever the page is resized
-        // add objects for display in background. draws each image added to the background once
-        function render() {
-            background.removeAllChildren();
+       // useful variables
+       var canvasWidth = app.canvas.width;
+       var canvasHeight = app.canvas.height;
+       var groundY = ground.y;
+      
+       // container which will be returned
+       var background;
+      
+       //////////////////////////////////////////////////////////////////
+       // ANIMATION VARIABLES HERE //////////////////////////////////////
+       //////////////////////////////////////////////////////////////////
+       // TODO (several):
+     var tree
+     var buildings = []
+    
+       // called at the start of game and whenever the page is resized
+       // add objects for display in background. draws each image added to the background once
+       function render() {
+           background.removeAllChildren();
 
-            // TODO 1:
-            // this currently fills the background with an obnoxious yellow;
-            // you should modify both the height and color to suit your$ game
-            var backgroundFill = draw.rect(canvasWidth,canvasHeight,'g reen');
-            background.addChild(backgroundFill);
-            
-            // TODO 2: - Add a moon and starfield
-            var moon = draw.bitmap("img/moon.png");
-        moon.x = 300;
-        moon.y = 200;
-        moon.scaleX = 7.0;
-        moon.scaleY = 5.0;
-        background.addChild(moon);
-              for(var i = 0; i <= 100; i++){
-            var circle = draw.circle(10, "white", "LightGray", 2);     
-            circle.x = canvasWidth * Math.random();
-            circle.y = canvasHeight * Math.random();
-            background.addChild(circle);
-             }
-            // TODO 4: Part 1 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
-          for (var i = 0; i < 6; ++i) {
-            var buildingHeight = 300;
-            var building = draw.rect(75, buildingHeight, "LightGray", "Black", 1);
-            building.x = 200 * i;
-            building.y = groundY - buildingHeight;
-            background.addChild(building);
-            buildings.push(building);
-}
-            
-            // TODO 3: Part 1 - Add a tree
+
+           // TODO 1:
+           // this currently fills the background with an obnoxious yellow;
+           // you should modify both the height and color to suit your game
+           var backgroundFill = draw.rect(canvasWidth,canvasHeight,'white');
+           background.addChild(backgroundFill);
+          
+           // TODO 2: - Add a moon and starfield
+           var moon = draw.bitmap("img/moon.png");
+               moon.x = 1000;
+               moon.y = -100;
+               moon.scaleX = 1.0;
+               moon.scaleY = 1.0;
+               background.addChild(moon);
+              
+               for(var i = 0; i<100; i++){
+               var circle = draw.circle(10, "white", "LightGray", 2);
+               circle.x = canvasWidth * Math.random();
+               circle.y = canvasHeight * Math.random();
+               background.addChild(circle);
+               }
+           // TODO 4: Part 1 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
+           for (var i = 0; i < 67; ++i) {
+               var buildingHeight = Math.random()*500;
+               var building = draw.rect(75, buildingHeight, "pink", "Black", 1);
+               building.x = 200 * i;
+               building.y = groundY - buildingHeight;
+               background.addChild(building);
+               buildings.push(building);
+           }  
+          
+           // TODO 3: Part 1 - Add a tree
            tree = draw.bitmap("img/tree.png");
-            tree.x = 0;
-            tree.y = 0;
-            background.addChild(tree);
-        } // end of render function - DO NOT DELETE
-        
-        
-        // Perform background animation
-        // called on each timer "tick" - 60 times per second
-        function update() {
-            // useful variables
-            var canvasWidth = app.canvas.width;
-            var canvasHeight = app.canvas.height;
-            var groundY = ground.y;
-            
-            // TODO 3: Part 2 - Move the tree!
-            tree.x = tree.x - 1;
+           tree.x = 500;
+           tree.y = groundY - 250;
+           background.addChild(tree);
+          
+       } // end of render function - DO NOT DELETE
+      
+      
+       // Perform background animation
+       // called on each timer "tick" - 60 times per second
+       function update() {
+           // useful variables
+           var canvasWidth = app.canvas.width;
+           var canvasHeight = app.canvas.height;
+           var groundY = ground.y;
+          
+           // TODO 3: Part 2 - Move the tree!
+           tree.x = tree.x - 1;
 
-            if (tree.x < -200) {
-                tree.x = canvasWidth;
-}
-            
-            // TODO 4: Part 2 - Parallax
-            for (var i = 0; i < buildings.length; i++) {
-                var eachElement = building[i];
-                eachElement.x = eachElement.x -10.0;
 
-                if (eachElement.x < -200){
-                    eachElement.x = canvasWidth;
-                }
+           if (tree.x < -200) {
+               tree.x = canvasWidth;
+           }
+           
+          
+          
+           // TODO 4: Part 2 - Parallax
+           for (var i = 0; i < buildings.length; i++) {
+               var eachElement = buildings[i];
+           eachElement.x = eachElement.x - 0.5;
 
-}
 
-        } // end of update function - DO NOT DELETE
-        
-        
-        
-        /* Make a createjs Container for the background and let it know about the render and upate functions*/
-        background = new createjs.Container();
-        background.resize = render;
-        background.update = update;
-        
-        /* make the background able to respond to resizing and timer updates*/
-        app.addResizeable(background);
-        app.addUpdateable(background);
-        
-        /* render and return the background */
-        render();
-        return background;
-    };
+           if (eachElement.x < -200) {
+               eachElement.x = canvasWidth;
+           }
+           }
+
+
+       } // end of update function - DO NOT DELETE
+      
+      
+      
+       /* Make a createjs Container for the background and let it know about the render and upate functions*/
+       background = new createjs.Container();
+       background.resize = render;
+       background.update = update;
+      
+       /* make the background able to respond to resizing and timer updates*/
+       app.addResizeable(background);
+       app.addUpdateable(background);
+      
+       /* render and return the background */
+       render();
+       return background;
+   };
 };
+
 
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
 if((typeof process !== 'undefined') &&
-    (typeof process.versions.node !== 'undefined')) {
-    // here, export any references you need for tests //
-    module.exports = background;
+   (typeof process.versions.node !== 'undefined')) {
+   // here, export any references you need for tests //
+   module.exports = background;
 }
+
+
+
